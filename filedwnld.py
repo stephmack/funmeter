b69613c7b711bc5bd59f54a3ed65884a1fa990e3
stephmack <mackey.stephen@gmail.com>
2015-10-28 11:57:43 +0000

    Update

--- filedwnld.py
+++ filedwnld.py
@@ -4,8 +4,7 @@ import MySQLdb
 import warnings
 
 warnings.filterwarnings("error")
-os.chdir('/home/pi/EnergyMon/py')
-os.system('sudo git clone git://github.com/stephmack/funmeter.git')
+os.system('sudo git clone git://github.com/stephmack/funmeter.git /home/pi/EnergyMon/py/funmeter')
 with open('/home/pi/EnergyMon/py/funmeter/install.csv', 'r') as f:
      for read_data in f.readlines():
      #read_data = f.readline()
